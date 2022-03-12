d396e09bd7079d464d52aab008c1bdeab532fccf
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-12 15:19:22 +0100

    Adding the jobs dict

--- app/app.py
+++ app/app.py
@@ -5,13 +5,24 @@ from random import randint
 app = Flask(__name__)
 app.config.from_object('app.config')
 
+jobs = {
+    "pilot": {"name": "Pilot", "income": "8000", "tax": "3000"},
+    "sanitor": {"name": "Sanitor", "income": "1500", "tax": "300"},
+    "teacher": {"name": "Teacher", "income": "3000", "tax": "1000"},
+    "techy": {"name": "Software Enigneer", "income": "10000", "tax": "4000"}
+}
+
 @app.route('/landing')
 def landing():
     return render_template("landing.html")
 
-@app.route('/playing')
-def playing():
-    return render_template("playing.html")
+# @app.route('/playing')
+# def playing():
+#     return render_template("playing.html")
+
+@app.route('/playing/<slug>')
+def playing(slug):
+    return slug
 
 @app.route('/login')
 def login():
