4352d315bc85b0d2dd372a0f7fb170b5d9f9287f
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-15 23:14:59 +0100

    Added content tests for root and login

--- app/tests/simple_pages/test_routes.py
+++ app/tests/simple_pages/test_routes.py
@@ -3,6 +3,11 @@ def test_root_success(client):
     response = client.get('/')
     assert response.status_code == 200
 
+def test_root_content(client):
+    # Returns are you sick text
+    response = client.get('/')
+    assert b'Are you sick of recalculating your cashflow round by round' in response.data
+
 def test_home_redirect(client):
     # Page redirects to root
     response = client.get('/home')
@@ -12,3 +17,8 @@ def test_login_success(client):
     # Page loads
     response = client.get('/login')
     assert response.status_code == 200
+
+def test_login_content(client):
+    # Returns login text
+    response = client.get('/login')
+    assert b'You can log in to save and look up your recent gameplays' in response.data
