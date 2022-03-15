cf597db17dc13d808bae697295838517ddb3e5dc
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-15 23:24:26 +0100

    Added test for dynamic route

--- app/tests/jobs/test_jobs_routes.py
+++ app/tests/jobs/test_jobs_routes.py
@@ -1,3 +1,5 @@
+from app.jobs.routes import jobs
+
 def test_choose_job_success(client):
     # Page loads
     response = client.get('/jobs')
@@ -7,3 +9,9 @@ def test_play_redirect(client):
     # Page redirects to /jobs
     response = client.get('/play')
     assert response.status_code == 302
+
+def test_choose_job_success(client):
+    # Every dynamic page loads
+    for job in jobs:
+        response = client.get(f'/jobs/{job}')
+        assert response.status_code == 200
