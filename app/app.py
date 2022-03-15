73c2b63cd150c8a413dcc3a9410944b2b82d2229
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-16 00:24:09 +0100

    Set up SWL Alchemy, SQLite

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from atexit import register
 from flask import Flask, redirect, url_for, render_template, send_file
 from . import jobs, simple_pages
+from app.extensions.database import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')
 
+    register_extensions(app)
     register_blueprints(app)
 
     return app
@@ -14,3 +16,7 @@ def create_app():
 def register_blueprints(app: Flask):
     app.register_blueprint(jobs.routes.blueprint)
     app.register_blueprint(simple_pages.routes.blueprint)
+
+# Extensions
+def register_extensions(app: Flask):
+  db.init_app(app)
