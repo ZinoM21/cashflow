cba6b18a573c8d5fd53127119612cddf5500cf7b
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-16 10:52:40 +0100

    Adding migration to tests

--- app/jobs/models.py
+++ app/jobs/models.py
@@ -20,3 +20,12 @@ class Job(db.Model):
     car_loans = db.Column(db.Numeric(10, 0))
     credit_card_debt = db.Column(db.Numeric(10, 0))
     payday = db.Column(db.Numeric(10, 0))
+
+# If something was changed in the db, run a migration like this:
+    # flask db migrate -m 'add picture_url to Cookie'
+    # flask db upgrade
+    # Important: Don't change the name of a property or remove and add properties within the same migration. Changing existing column names or removing one column and adding another within the same migration can cause issues with SQLite. This is different from other databases. But SQLite doesn't support just changing existing columns. So what you'd have to do is to use separate migrations: one for removing a column and another for adding a new column. (☝️ Note that this will mean the data within a deleted column will be lost!)
+
+# If you want to downgrade to another migration, use this line in terminal:
+    # flask db downgrade
+    
