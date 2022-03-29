aeabb6805836d7da8760ad9f437a7bb58ff6b593
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-29 14:25:48 +0200

    Update: add CRUDMixin class

--- app/jobs/models.py
+++ app/jobs/models.py
@@ -1,6 +1,6 @@
-from app.extensions.database import db
+from app.extensions.database import db, CRUDMixin
 
-class Job(db.Model):
+class Job(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(80), unique=True)
     name = db.Column(db.String(20))
