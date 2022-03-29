dd9e1bfb71cbd06e1bb8aa7bd5fd01c947120d66
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-29 14:47:33 +0200

    Added and test for CRUDMixin delete() method

--- app/extensions/database.py
+++ app/extensions/database.py
@@ -6,7 +6,12 @@ migrate = Migrate()
 
 class CRUDMixin():
 
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
-    return self
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
--- app/tests/jobs/test_models.py
+++ app/tests/jobs/test_models.py
@@ -31,3 +31,33 @@ def test_job_update(client):
 
     updated_job = Job.query.filter_by(slug="mech").first()
     assert updated_job.name == "Doc"
+
+def test_job_delete(client):
+    # deletes job
+    test_job = Job(
+        slug="mech",
+        name="Mech", 
+        salary=123, 
+        cashflow=234, 
+        tax=345, 
+        home_mortgage_payment=456, 
+        school_loan_payment=567, 
+        car_loan_payment=678, 
+        credit_card_payment=789, 
+        other_expenses=890, 
+        bank_loan_payment=1235, 
+        children=4236,
+        savings=3467, 
+        home_mortgage=34543543, 
+        school_loans=4737, 
+        car_loans=546345, 
+        credit_card_debt=76365, 
+        payday=675675
+        )
+    db.session.add(test_job)
+    db.session.commit()
+
+    test_job.delete()
+
+    deleted_job = Job.query.filter_by(slug="mech").first()
+    assert deleted_job is None
