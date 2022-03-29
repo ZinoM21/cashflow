7966d0bcc90f8831cfad69283feff2d0e3b5ccbf
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-29 13:32:38 +0200

    Fixed issue with dotenv

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 from os import environ
 from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 load_dotenv()
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
