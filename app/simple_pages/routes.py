420045b25dad3e73e7b7f99a01f991267eef531a
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-29 22:21:57 +0200

    Import request & change form action route

--- app/simple_pages/routes.py
+++ app/simple_pages/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request
 
 blueprint = Blueprint('simple_pages', __name__)
 
