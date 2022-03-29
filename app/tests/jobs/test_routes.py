a3172f586dd98ce55830b9f021fad12f68a2daf6
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-29 22:31:57 +0200

    Removed dynamic jobs routes test

--- app/tests/jobs/test_routes.py
+++ app/tests/jobs/test_routes.py
@@ -1,5 +1,3 @@
-from app.jobs.routes import jobs
-
 def test_choose_job_success(client):
     # Page loads
     response = client.get('/jobs')
@@ -9,9 +7,3 @@ def test_play_redirect(client):
     # Page redirects to /jobs
     response = client.get('/play')
     assert response.status_code == 302
-
-def test_choose_job_success(client):
-    # Every dynamic page loads
-    for job in jobs:
-        response = client.get(f'/jobs/{job}')
-        assert response.status_code == 200
