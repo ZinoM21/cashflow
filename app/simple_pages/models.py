3311abefaa547e2c05052d53dc901c6e6cd623b0
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-30 14:35:51 +0200

    Re-migrated because of column change in User

--- app/simple_pages/models.py
+++ app/simple_pages/models.py
@@ -2,5 +2,5 @@ from app.extensions.database import db, CRUDMixin
 
 class User(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80))
--- app/simple_pages/services/create_user.py
+++ app/simple_pages/services/create_user.py
@@ -3,7 +3,7 @@ from app.simple_pages.models import User
 def create_user(form_data):
     # Create new User 
     new_user = User(
-        username=form_data.get('username'),
+        email=form_data.get('email'),
         password=form_data.get('password')
     )
     new_user.save()
