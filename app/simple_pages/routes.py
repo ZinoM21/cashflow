4290a002d495c31b6d2136625c7778b306fd4c1e
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-30 23:24:20 +0200

    Exceptions & Logging Errors

--- app/simple_pages/routes.py
+++ app/simple_pages/routes.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, redirect, render_template, url_for, request
+from flask import Blueprint, redirect, render_template, url_for, request, current_app
 from app.simple_pages.services.create_user import create_user
 
 blueprint = Blueprint('simple_pages', __name__)
@@ -47,12 +47,19 @@ def get_account():
 def post_account():
     # Log:
     print('YOUR DATA WAS UPLOADED')
-    
-    if not all([
-        request.form.get('email'),
-        request.form.get('password')
-    ]):
-        return render_template('simple_pages/signup.html', error="Please fill out all input fields to sign up!")
+    try:
+        if not all([
+            request.form.get('email'),
+            request.form.get('password')
+        ]):
+            raise Exception("Please fill out all input fields to sign up!")
 
-    create_user(request.form)
-    return render_template("simple_pages/account.html")
+        create_user(request.form)
+        return render_template("simple_pages/account.html")
+
+    except Exception as error_message:
+        error = error_message or 'An error occurred while signing up. Please make sure to enter valid data.'
+        
+        current_app.logger.info(f'Error sign up: {error}')
+
+        return render_template('simple_pages/signup.html', error=error)
