d597d27a1659804fcdb3cffdc007f848958066f7
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-03-31 12:39:38 +0200

    Changed to make postgres work in Heroku

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
