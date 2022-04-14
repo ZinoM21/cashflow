40a8ec875b985bbc7e47107a552cb48bba070413
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-04-14 17:02:12 +0200

    Added .unauthorized_handler

--- app/extensions/authentication.py
+++ app/extensions/authentication.py
@@ -1,9 +1,14 @@
+from flask import redirect, url_for
 from flask_login import LoginManager
 from app.users.models import User
-
 
 login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(user_id)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    
+    return redirect(url_for('users.get_login'))
