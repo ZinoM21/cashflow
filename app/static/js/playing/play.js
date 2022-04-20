ac6a205a2fe7c7fb0295f8b12ac7501b9cbc50dc
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-04-20 17:09:36 +0200

    Added play.js

---
+++ app/static/js/playing/play.js
@@ -0,0 +1,9 @@
+// Start game
+let AddStartingView = () => {
+    const h1 = document.createElement("h1");
+    // h1.setAttribute("class", "generatedErrors")
+    h1.innerText = "Let's Start the Game!";
+    document.getElementById("mainSection").appendChild(newP);
+}
+
+AddStartingView()
