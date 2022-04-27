e8f2c9cad466bd5d76605572b3e8c3f80680c717
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-04-27 15:28:08 +0200

    Added /setup route + styling

--- app/playing/routes.py
+++ app/playing/routes.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, redirect, render_template, url_for, request
+from app.professions.models import Profession
 
 blueprint = Blueprint('playing', __name__)
 
-### START THE GAME ###
+### SETUP THE GAME ###
+@blueprint.route('/setup')
+def setup():
 
+    all_profs = Profession.query.all()
+    prof_names = []
+    for prof in all_profs:
+        prof_names.append(prof.name)
+
+    return render_template("playing/setup.html", all_profs=all_profs, prof_names=prof_names)
+
+
+### START THE GAME ###
 @blueprint.route('/play')
 def play():
     return render_template("playing/play.html")
