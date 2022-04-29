bbbc8282b0b7cf68ae0fe56db4dba6f7d78972a9
Zino <100772322+ZinoM21@users.noreply.github.com>
2022-04-29 14:14:27 +0200

    Make onepager from /setup & /play

--- app/playing/routes.py
+++ app/playing/routes.py
@@ -3,19 +3,7 @@ from app.professions.models import Profession
 
 blueprint = Blueprint('playing', __name__)
 
-### SETUP THE GAME ###
-@blueprint.route('/setup')
-def setup():
-
-    all_profs = Profession.query.all()
-    prof_names = []
-    for prof in all_profs:
-        prof_names.append(prof.name)
-
-    return render_template("playing/setup.html", all_profs=all_profs, prof_names=prof_names)
-
-
 ### START THE GAME ###
 @blueprint.route('/play')
-def play():
+def get_play():
     return render_template("playing/play.html")
