39555835d510133b0a9c7d7ba633db1cfadf7659
Zino Meyer <zino.meyer@code.berlin>
2022-05-16 17:36:04 +0200

    CSS, JS & HTML for popup & PAY btn & charity

--- app/static/js/playing/play.js
+++ app/static/js/playing/play.js
@@ -39,4 +39,69 @@ takeout.addEventListener("click", (e) => {
     player.takeOutLoan();
 });
 
+pay.addEventListener("click", (e) => {
+    popupPay(createPopUpContainer());
+    let popupContainerBG = document.getElementById("popupPayContainerBG")
+    let charityButton = document.getElementById("charityButton");
+    let cancelButton = document.getElementById("cancelButton");
+
+    cancelButton.onclick = () => {
+        popupContainerBG.remove()
+        render_stats(player);
+    };
+
+    charityButton.onclick = () => {
+        player.addToLedger("Charity Donation", player.getTotalIncome() * 0.1 * (-1));
+        popupContainerBG.remove()
+        render_stats(player);
+    };
+});
+
+let createPopUpContainer = () => {
+    let popupContainerBG = document.createElement("div");
+    popupContainerBG.setAttribute("class", "popupContainerBG");
+    popupContainerBG.setAttribute("id", "popupPayContainerBG");
+    document.querySelector("main").appendChild(popupContainerBG);
+    
+    let popupContainer = document.createElement("div");
+    popupContainer.setAttribute("class", "popupContainer");
+    popupContainerBG.appendChild(popupContainer);
+
+    let cancelButton = document.createElement("div");
+    cancelButton.setAttribute("id", "cancelButton");
+    popupContainer.appendChild(cancelButton);
+
+    return popupContainer
+};
+
+let popupPay = (popupContainer) => {
+    let heading = document.createElement("h1");
+    heading.innerText = "Pay";
+    popupContainer.appendChild(heading);
+
+    let charityButton = document.createElement("button");
+    charityButton.setAttribute("class", "button");
+    charityButton.classList.add("popupButton");
+    charityButton.setAttribute("id", "charityButton");
+    charityButton.innerText = "Charity";
+    popupContainer.appendChild(charityButton);
+
+    let lendmoneyButton = document.createElement("button");
+    lendmoneyButton.setAttribute("class", "button");
+    lendmoneyButton.classList.add("popupButton");
+    lendmoneyButton.innerText = "Lend Money";
+    popupContainer.appendChild(lendmoneyButton);
+
+    let paymoneyButton = document.createElement("button");
+    paymoneyButton.setAttribute("class", "button");
+    paymoneyButton.classList.add("popupButton");
+    paymoneyButton.innerText = "Pay Money";
+    popupContainer.appendChild(paymoneyButton);
+};
+
+let removeAllWithClass = (className) => {
+    document.querySelectorAll(className).forEach(e => e.remove())
+};
+
+
 export {market_action, buy, sell, pay, collect, downsized, doodad, takeout, payoff}
